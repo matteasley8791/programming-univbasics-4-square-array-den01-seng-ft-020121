ef0939adba9daa56b4c6d5ee85f2da2dc4dc0bc6
Matthew Easley <matteasley8791@gmail.com>
2021-01-14 21:10:15 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(numbers)
   counter = 0 
   sqr_numbers = []
   while numbers[counter] do 
-    sqr_numbers (numbers.sqrt())
+    sqr_numbers <<(numbers.sqrt())
   counter += 1 
   end
   new_array
